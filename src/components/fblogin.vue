b5c21f114e0ad29673c7405e63e2ecd5e4250ba3
shawn70020 <shawn70020@gmail.com>
2024-09-20 22:27:27 +0800

    檢查雙重認證-測試2

--- src/components/fblogin.vue
+++ src/components/fblogin.vue
@@ -8,24 +8,15 @@
 import { onMounted, ref } from "vue";
 import { defineEmits } from "vue"; // Vue 3 中用於定義事件
 import { useAppStore } from "../stores/index.js";
+import { fbInitPromise } from '../main'; // 引入 fbInitPromise
 export default {
   setup(props, { emit }) {
     const appStore = useAppStore();
     const userData = ref(null);
 
-    // onMounted(() => {
-    //   // 初始化 Facebook SDK
-    //   window.fbAsyncInit = function () {
-    //     FB.init({
-    //       appId: "1214989089541346", // 這裡要替換為你的 Facebook 應用 ID
-    //       cookie: true,
-    //       xfbml: true,
-    //       version: "v16.0",
-    //     });
-    //   };
-    // });
+    const loginWithFacebook = async () => {
+      await fbInitPromise()
 
-    const loginWithFacebook = () => {
       FB.login(
         (response) => {
           if (response.authResponse) {
--- src/main.js
+++ src/main.js
@@ -6,28 +6,30 @@ import router from '../src/router'
 
 const pinia = createPinia();
 
-// Facebook SDK 初始化
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: "1214989089541346", // 替換為你的 Facebook 應用 ID
-    cookie: true,
-    xfbml: true,
-    version: "v16.0",
-  });
-};
+export const fbInitPromise = new Promise((resolve) => {
+  window.fbAsyncInit = function () {
+    FB.init({
+      appId: "1214989089541346", // 替換為你的 Facebook 應用 ID
+      cookie: true,
+      xfbml: true,
+      version: "v16.0",
+    });
+    resolve(); // SDK 初始化完成，解決 Promise
+  };
 
-// 加載 Facebook SDK
-(function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "facebook-jssdk");
+  // 加載 Facebook SDK
+  (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+      return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
+});
 
 createApp(App)
   .use(router)
