070ac9d5f1ff7ad3ee92dcedd028177790da47c6
shawn70020 <shawn70020@gmail.com>
2024-09-21 20:20:58 +0800

    cookie寫法

--- src/stores/index.js
+++ src/stores/index.js
@@ -15,16 +15,16 @@ export const useAppStore = defineStore('app', {
     actions: {
         setFbUserData(data) {
             this.fbUserData = data; // 設置 Facebook 用戶資料
-            Cookies.set('fbStatus', data, { expires: 7 });
+            Cookies.set('fbData', JSON.stringify(data), { expires: 7 });
         },
         setGoogleUserData(data) {
             this.googleUserData = data; // 設置 Google 用戶資料
-            Cookies.set('googleStatus', data, { expires: 7 });
+            Cookies.set('googleData', JSON.stringify(data), { expires: 7 });
         },
         restoreUserData() {
             // 從 Cookie 中獲取資料並設置到 state 中
-            const fbData = Cookies.get('fbStatus');
-            const googleData = Cookies.get('googleStatus');
+            const fbData = Cookies.get('fbData');
+            const googleData = Cookies.get('googleData');
             this.fbUserData = fbData ? JSON.parse(fbData) : null;
             this.googleUserData = googleData ? JSON.parse(googleData) : null;
         },
